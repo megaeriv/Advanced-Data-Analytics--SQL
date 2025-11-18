bea81442cb41f4c6938030c4a1f7464f46c79baf
Erivona Mega <megaerivona@gmail.com>
2025-11-18 22:21:44 +0000

    Create Change over Time.sql

---
+++ scripts/Change over Time.sql
@@ -0,0 +1,12 @@
+---------------------------------------------
+-- CHANGE OVER TIME
+---------------------------------------------
+-- A. Analyze Sales performance over time
+SELECT
+	YEAR(order_date) AS order_year,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)
+ORDER BY YEAR(order_date);
