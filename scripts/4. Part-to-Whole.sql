03e26014cc2ebd0cec38a0efb8c0490c6181f5eb
Erivona Mega <megaerivona@gmail.com>
2025-11-19 22:20:04 +0000

    Update 4. Part-to-Whole.sql

--- scripts/4. Part-to-Whole.sql
+++ scripts/4. Part-to-Whole.sql
@@ -1,6 +1,20 @@
 /*
 ---------------------------------------------
--- PART TO WHOLE
+-- PART TO WHOLE (Proportion)
 ---------------------------------------------
-
 */
+WITH sales_by_category AS (
+    SELECT
+        p.category,
+        SUM(s.sales_amount) AS product_sales
+    FROM gold.fact_sales s
+    LEFT JOIN gold.dim_products p
+        ON s.product_key = p.product_key
+    GROUP BY p.category
+)
+SELECT
+    category,
+    product_sales,
+    CONCAT(ROUND(CAST(product_sales AS FLOAT)/ SUM(product_sales) OVER () * 100.0 ,2), '%') AS percentage_of_sales
+FROM sales_by_category
+ORDER BY product_sales DESC;
