1b8304c316d11dc785b0ab1e776b412a8f02b807
Erivona Mega <megaerivona@gmail.com>
2025-11-19 15:59:03 +0000

    Update 1. Change over Time.sql

--- scripts/1. Change over Time.sql
+++ scripts/1. Change over Time.sql
@@ -6,12 +6,14 @@ This analysis focuses on measuring of how measures eveolves over time,
 helping undertand trends and seasonality
 */
 
--- A. Analyze Sales performance over time
+-- A. Analyze Sales, Customer performance over time
 SELECT
-	YEAR(order_date) AS order_year,
+	YEAR(order_date)AS order_year,
+	DATENAME(MONTH,order_date) AS order_month,
 	SUM(sales_amount) AS total_sales,
-	COUNT(DISTINCT customer_key) AS total_customers
+	COUNT(DISTINCT customer_key) AS total_customers,
+	sum(quantity) AS total_quantity
 FROM gold.fact_sales
 WHERE order_date IS NOT NULL
-GROUP BY YEAR(order_date)
-ORDER BY YEAR(order_date);
+GROUP BY YEAR(order_date), DATENAME(MONTH,order_date)
+ORDER BY YEAR(order_date), DATENAME(MONTH,order_date);
