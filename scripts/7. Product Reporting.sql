373395343188f566ef34458aa4f2b3f2dcefff3d
Erivona Mega <megaerivona@gmail.com>
2025-11-20 19:46:31 +0000

    Create 7. Product Reporting.sql

---
+++ scripts/7. Product Reporting.sql
@@ -0,0 +1,35 @@
+/*
+===============================================================================
+Product Report
+===============================================================================
+Purpose:
+    - This report consolidates key product metrics and behaviors.
+
+Highlights:
+    1. Gathers essential fields such as product name, category, subcategory, and cost.
+    2. Segments products by revenue to identify High-Performers, Mid-Range, or Low-Performers.
+    3. Aggregates product-level metrics:
+       - total orders
+       - total sales
+       - total quantity sold
+       - total customers (unique)
+       - lifespan (in months)
+    4. Calculates valuable KPIs:
+       - recency (months since last sale)
+       - average order revenue (AOR)
+       - average monthly revenue
+===============================================================================
+*/
+-- =============================================================================
+-- Create Report: gold.report_products
+-- =============================================================================
+IF OBJECT_ID('gold.report_products', 'V') IS NOT NULL
+    DROP VIEW gold.report_products;
+GO
+
+CREATE VIEW gold.report_products AS
+
+WITH base_query AS (
+/*---------------------------------------------------------------------------
+1) Base Query: Retrieves core columns from fact_sales and dim_products
+---------------------------------------------------------------------------*/
